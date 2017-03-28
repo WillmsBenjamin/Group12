58138110fdf66248058354877707559abccc57b0
KevenLiu <kevenliu94@gmail.com>
2017-03-28 17:18:57 -0400

    Update functions

--- DesktopTAMAS/src/ecse321/group12/tamas/controller/DepartmentRegisteredException.java
+++
@@ -1,8 +0,0 @@
-package ecse321.group12.tamas.controller;
-
-public class DepartmentRegisteredException extends Exception {
-	private static final long serialVersionUID = -5633928562703837868L;
-    public DepartmentRegisteredException(String errorMessage) {
-        super(errorMessage);
-    }
-}
